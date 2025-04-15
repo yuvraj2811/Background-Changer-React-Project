76c0fbad53f16cc09aa7c035787be5d5c097ab5c
Gaurav Yuvraj <gyuvraaj123@gmail.com>
2025-04-15 17:57:29 +0530

    All files

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-  base: "/Background-Changer-React-Project",
+  base: "/Background-Changer-React-Project/",
 })
